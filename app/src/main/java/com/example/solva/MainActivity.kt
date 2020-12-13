c9c1ccf388f5c33c57a9bed517018c233f4d13b7
devimumo <43302770+devimumo@users.noreply.github.com>
2020-12-13 10:42:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/solva/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.solva
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import com.example.solva.SpareParts.Spares_Search_Dashboard
+import java.io.Serializable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+    fun navigation_onclick(view: View) {
+
+        when (view.id) {
+            R.id.spare_parts -> {
+
+
+                val intent = Intent(this, Spares_Search_Dashboard::class.java)
+                val bundle = Bundle()
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/solva/SpareParts/Spares_Search_Dashboard.kt
@@ -0,0 +1,12 @@
+package com.example.solva.SpareParts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.solva.R
+
+class Spares_Search_Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spares__search__dashboard)
+    }
+}
